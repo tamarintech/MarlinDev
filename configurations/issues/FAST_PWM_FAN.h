881db4aede2a5d93425f5ee20021b27d2c560015
Richard Wackerbarth <rkw@dataplex.net>
2015-10-26 07:27:10 -0400

    Test for issue #248 - FAST_PWM_FAN

---
+++ configurations/issues/FAST_PWM_FAN.h
@@ -0,0 +1,5 @@
+// Unbalanced brackets in Marlin_main.cpp when FAST_PWM_FAN is selected
+
+#define FAST_PWM_FAN
+
+#include "configurations/transitional_default_configurations/ramps/Configuration.h"
