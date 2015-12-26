c0ef611726ef509d4e9d1a0ef23e8312ef291d41
Richard Wackerbarth <rkw@dataplex.net>
2015-12-26 15:08:21 -0600

    Provide a Honeypot for users who fail to follow instructions

---
+++ FirmwareCustomization.h
@@ -0,0 +1,9 @@
+// Sanity check
+
+// This file is provided to catch users who fail to select an appropriate board from the Tools menu.
+
+#ifndef MARLIN
+  #error Compilation using cores other than those provided for this distribution are unsupported
+  #error PLEASE select a Board from the Marlin AVR section of the Tools menu.
+#endif
+
--- pins_RAMPS_13.h
+++ pins_RAMPS_13.h
@@ -23,7 +23,7 @@
  */
 
 #if !defined(__AVR_ATmega1280__) && !defined(__AVR_ATmega2560__)
-  #error Oops!  Make sure you have 'Arduino Mega' selected from the 'Tools -> Boards' menu.
+  #error Oops!  Make sure you have 'Arduino Mega' selected from the Marlin AVR section of the 'Tools -> Boards' menu.
 #endif
 
 #define LARGE_FLASH true
